cd3ed3ef45462cb5ac4b9f0c8e30a2b3ec592d51
John Papa <papa@Johns-MBP.lan>
2019-12-06 10:39:54 -0500

    added 25dos functions for issues and prs

---
+++ SayThankYouForIssue25DOS/index.ts
@@ -0,0 +1,43 @@
+import { AzureFunction, Context, HttpRequest } from '@azure/functions';
+import { IssuesCreateCommentParams } from '@octokit/rest';
+import * as Octokit from '@octokit/rest';
+
+const httpTrigger: AzureFunction = async function(context: Context, req: HttpRequest): Promise<void> {
+  const { body: payload } = req;
+  context.log(payload);
+
+  const repo = payload.repository.name;
+  const owner = payload.repository.owner.login;
+  const issue_number = payload.issue.number;
+  const user = payload.issue.user.login;
+  const action = payload.action;
+
+  let body = 'Nothing to see here';
+
+  if (action === 'opened') {
+    body = `From all of us on the Cloud Advocacy team at Microsoft, thank you @${user} for creating this issue!\n\nHave a Happy Holiday season!`;
+    // body = `Thank you @${user} for creating this issue!\n\nHave a Happy Holiday season!`;
+    context.log(body);
+    const comment: IssuesCreateCommentParams = {
+      repo,
+      owner,
+      issue_number,
+      body
+    };
+    await createIssueComment(comment);
+  }
+
+  context.res = {
+    status: 200 /* Defaults to 200 */,
+    body
+  };
+};
+
+async function createIssueComment(comment: IssuesCreateCommentParams) {
+  const auth = process.env.githubkey_25dos;
+  const octokit = new Octokit({ auth });
+  const response = await octokit.issues.createComment(comment);
+  return response;
+}
+
+export default httpTrigger;
